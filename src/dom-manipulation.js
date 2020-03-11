09d2d30a1f04fc81d2d3b54b9f8841310d8bc27d
alexawesomecode <alexawesomecode@gmail.com>
2020-03-11 16:18:33 -0400

    [Feature] Added first part of edit functionality

--- src/dom-manipulation.js
+++ src/dom-manipulation.js
@@ -29,6 +29,7 @@ const appendTask = (elem, ind) => {
       <h2>${elem.title}</h2>
   </div>
   <p>Due: <span>${elem.date}</span>|<span>${elem.time}</span></p>
+
   `;
   parent.appendChild(newElem);
 };
@@ -87,8 +88,10 @@ const addTasks = () => {
 const completeTask = () => {
   const checkboxes = document.querySelectorAll("input[type='checkbox']");
   console.log(checkboxes);
-  checkboxes.forEach(box => {
-    box.onclick = () => {
+    checkboxes.forEach(box => {
+    
+     box.onclick = () => {
+               
       if (box.checked == true) {
         const parent = box.parentNode
         const ind = parent.dataset.index;
@@ -96,14 +99,79 @@ const completeTask = () => {
         parent.parentNode.style.borderTop = "2px solid green"
         parent.parentNode.style.color = "gray"
         // parent.parentNode.style.setProperty('--width', `${100}%`)
-        console.log(ind);
+          console.log(ind);
+        editTask(2);
       } else {
         parent.parentNode.style.borderTop = "2px solid black"
         parent.parentNode.style.backgroundColor = "rgba(252, 87, 101, 0.829)"
         parent.parentNode.style.color = "#000"
+
+          
+          
       }
     };
   });
 };
+
+const editTask = (index) => {
+
+    const task = retrieveTasks().array[index]
+    console.log(task)
+    let formBase = ` <form action="" method="post" class="edit-form">
+          <div class="choose-project">
+            <label for="project">Choose Existing Project</label>
+            <select id="project" name="project">
+              <option value="">${task.project}</option>
+            </select>
+            <span>or</span>
+            <div>
+              <label for="newProj">Add new Project</label>
+              <input type="text" name="newProj" id="newProj">
+            </div>
+          </div>
+          <div>
+            <label for="title">Task Title</label>
+            <input type="text" name="title" id="title" value="${task.title}">
+          </div>
+          <div>
+            <label for="title">Task Description</label>
+            <input type="text" name="description" id="description" value="${task.description}">
+          </div>
+          <div>
+            <label for="dueDate">Task Due-Date</label>
+            <input type="date" name="dueDate" id="dueDate" class="border" value="${task.date}">
+          </div>
+          <div>
+            <label for="dueTime">Task Due-Time</label>
+            <input type="time" name="dueTime" id="dueTime" class="border" value="${task.time}">
+          </div>
+          <div>
+            <p>Task Priority</p>
+            <label for="urgent">Urgent</label>
+            <input type="radio" name="priority" id="urgent" class="radio border">
+            <label for="important">Important</label>
+            <input type="radio" name="priority" id="important" class="radio border">
+            <label for="lessImportant">Less Important</label>
+            <input type="radio" name="priority" id="lessImportant" class="radio border">
+          </div>
+          <div>
+            <label for="notes">Add Notes</label>
+            <input type="text" name="notes" id="notes" value="${task.notes}">
+          </div>
+          <input type="submit" value="Save Changes">
+        </form>
+        `;
+    console.log(index)
+    const parent = document.querySelector("body");
+    const formHidden = document.createElement('div');
+    formHidden.classList.add('show-form');
+    formHidden.innerHTML = formBase;
+    parent.appendChild(formHidden)
+    console.log(formHidden)
+//    formHidden.classList.remove('edit-hidden');
+    
+    
+};
+
 
 export { addTasks, appendStorage, appendProjects, completeTask };
